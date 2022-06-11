2338a0cfa1c6606c07094388b1fe67ef403aff1d
Saudcodes <saudstunts@gmail.com>
2022-06-11 16:50:16 +0400

    remove links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,23 +34,8 @@ export default function Navbar(props) {
               <li className="nav-item">
                 <a className="nav-link text-dark active" aria-current="page" href="/">{props.link}</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="/">Action</a></li>
-                  <li><a className="dropdown-item" href="/">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="/">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="/" tabIndex="-1" aria-disabled="true">Disabled</a>
-              </li>
+
+
             </ul>
             <button className="btn mx-1" type="submit">{btnText}</button>
 
