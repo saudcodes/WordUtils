ae0d5b4854a59a77a4cad10971965c32fa57140c
Saudcodes <saudstunts@gmail.com>
2022-06-13 14:52:46 +0400

    remove usestate method

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 const Counter = () => {
 
-    const [value, setValue] = useState(1) // usetate setsz the value of the variable, to change value of var use function "useState()"
+    const [value, setValue] = useState(1)
 
     const increament = () => {
 
