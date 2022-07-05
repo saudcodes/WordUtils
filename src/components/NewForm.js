96b9ce57a0c14d354e17adfb0279be1bed515341
Saudcodes <saudstunts@gmail.com>
2022-07-06 03:14:02 +0400

    prevent input area change

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -32,19 +32,19 @@ const NewForm = (props) => {
         setText(a)
         props.showAlert("success", " Your Text Has been Capitalized");
     }
-    const handleActivateText = (e) => {
-        e.preventDefault()
-        setIsPreviewEnabled(true)
-        if (isPreviewEnabled === true) {
-            setIsPreviewEnabled(false)
+    // const handleActivateText = (e) => {
+    //     e.preventDefault()
+    //     setIsPreviewEnabled(true)
+    //     if (isPreviewEnabled === true) {
+    //         setIsPreviewEnabled(false)
 
-            props.showAlert("success", " Text Preview has been Disabled!!!");
+    //         props.showAlert("success", " Text Preview has been Disabled!!!");
 
 
-        }
-        else { props.showAlert("success", " Text Preview has been Enabled!!!"); }
+    //     }
+    //     else { props.showAlert("success", " Text Preview has been Enabled!!!"); }
 
-    }
+    // }
     const handleCopyText = (text) => {
         text.preventDefault()
         text = document.getElementById("myTextArea")
@@ -71,7 +71,7 @@ const NewForm = (props) => {
                     <div className="mt-5">
                         <h3 className={`fst-italic text-${props.mode === "light" ? "dark" : "light"}`}> {text.length === 0 ? "" : text.length + " charactars " + text.split(" ").length + " words"}</h3>
                         <label htmlFor="validationTextarea" className={`form-label text-${props.mode === "light" ? "dark" : "light"}`}></label>
-                        <textarea value={text} onChange={handleChange} className="form-control  is-invalid" rows="4" id="myTextArea" placeholder="Required example textarea" required></textarea>
+                        <textarea onChange={handleChange} className="form-control  is-invalid" rows="4" id="myTextArea" placeholder="Required example textarea" required></textarea>
                         <div className="invalid-feedback my-1">
                             {text.length === 0 ? "Enter some text in the box " : ""}
                         </div>
@@ -81,7 +81,7 @@ const NewForm = (props) => {
                         <button style={props.mode === "light" ? { color: "white" } : props.myColor} className="btn mx-2  btn-primary" onClick={handleUpperCase} type="submit">To uppercase</button>
                         <button style={props.mode === "light" ? { color: "white" } : props.myColor} className="btn mx-2 btn-primary" onClick={handleLowerCase} type="submit">To lowercase</button>
                         <button style={props.mode === "light" ? { color: "white" } : props.myColor} className="btn mx-2 btn-primary" onClick={handleCapitalize} type="submit">To capitalize</button>
-                        <button style={props.mode === "light" ? { color: "white" } : props.myColor} className="btn mx-2 btn-primary" onClick={handleActivateText} type="submit">{isPreviewEnabled === false ? "Enable Preview" : " Disable Preview"} </button>
+                        {/* <button style={props.mode === "light" ? { color: "white" } : props.myColor} className="btn mx-2 btn-primary" onClick={handleActivateText} type="submit">{isPreviewEnabled === false ? "Enable Preview" : " Disable Preview"} </button> */}
                         <button style={props.mode === "light" ? { color: "white" } : props.myColor} className="btn mx-2 btn-primary" onClick={handleCopyText} type="submit">Copy </button>
                     </div>
 
@@ -89,7 +89,7 @@ const NewForm = (props) => {
                 </form>
                 <div style={{ height: "20vh", overflow: "auto", }} className="container testing border border-3 mt-5 border-primary">
                     <h4 className={`text-${props.mode === "light" ? "dark" : "light"} text-decoration-underline fst-italic`}>Preview</h4>
-                    <p className={`text-${props.mode === "light" ? "dark" : "light"} fs-5`}>{isPreviewEnabled === false ? "Enable Preview to view Text" : preview} </p>
+                    <p className={`text-${props.mode === "light" ? "dark" : "light"} fs-5`}>{preview} </p>
 
                 </div>
             </div>
